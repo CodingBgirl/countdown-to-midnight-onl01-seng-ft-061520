2cdd0fa1757c94928ebe001416dd5dde20f6f9af
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-06-19 23:57:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@ def countdown(number)
   new_year_countdown = 10
   until new_year_countdown === 0
     return "#{number} SECOND(S)!"
-    new_year_countdown -= 1
+    number -= 1
   end
 end
 binding.pry
