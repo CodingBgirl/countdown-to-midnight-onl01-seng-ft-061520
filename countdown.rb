4d932c031227cf1b340769de96567c3ac4747d52
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-06-20 00:32:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 
 def countdown(number)
   new_year_countdown = 10
-  while new_year_countdown  <= 10
+  until new_year_countdown  == 0
     puts "#{number} SECOND(S)!"
     number -= 1
     return "HAPPY NEW YEAR!"
